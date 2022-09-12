48e27d508fb36919ceaea3616e7b623abd1b2936
LisetteDelgado <109176403+LisetteDelgado@users.noreply.github.com>
2022-09-12 11:11:45 -0500

    Update and rename trimmomatic.sh to 02_trimmomatic.sh

--- 02_trimmomatic.sh
+++ 02_trimmomatic.sh
@@ -15,7 +15,9 @@ for name in $(cat $File)
 
 do
 
-	java -jar /home/lisette/bioinformatic-tools/Trimmomatic-0.39/trimmomatic-0.39.jar PE -phred33 -threads 8 "$name"_sortmerna_fwd.fastq.gz "$name"_sortmerna_rev.fastq.gz "$name"_sortmerna_forward_paired.fq "$name"_sortmerna_forward_unpaired.fq "$name"_sortmerna_reverse_paired.fq "$name"_sortmerna_reverse_unpaired.fq ILLUMINACLIP:adapter.fa:2:30:10 SLIDINGWINDOW:5:20 MINLEN:50
+	java -jar /home/lisette/bioinformatic-tools/Trimmomatic-0.39/trimmomatic-0.39.jar PE -phred33 -threads 8 "$name"_sortmerna_fwd.fastq.gz /
+	"$name"_sortmerna_rev.fastq.gz "$name"_sortmerna_forward_paired.fq "$name"_sortmerna_forward_unpaired.fq "$name"_sortmerna_reverse_paired.fq /
+	"$name"_sortmerna_reverse_unpaired.fq ILLUMINACLIP:adapter.fa:2:30:10 SLIDINGWINDOW:5:20 MINLEN:50
 
 # gz output files
 pigz -p8 "$name"_sortmerna_forward_paired.fq
