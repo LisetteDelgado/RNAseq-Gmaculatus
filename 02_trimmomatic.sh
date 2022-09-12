2318bd43d282c8e65bec393f380ec15aac17a365
LisetteDelgado <109176403+LisetteDelgado@users.noreply.github.com>
2022-09-12 11:15:30 -0500

    Update 02_trimmomatic.sh

--- 02_trimmomatic.sh
+++ 02_trimmomatic.sh
@@ -18,6 +18,7 @@ do
 	java -jar /home/lisette/bioinformatic-tools/Trimmomatic-0.39/trimmomatic-0.39.jar PE -phred33 -threads 8 "$name"_sortmerna_fwd.fastq.gz /
 	"$name"_sortmerna_rev.fastq.gz "$name"_sortmerna_forward_paired.fq "$name"_sortmerna_forward_unpaired.fq "$name"_sortmerna_reverse_paired.fq /
 	"$name"_sortmerna_reverse_unpaired.fq ILLUMINACLIP:adapter.fa:2:30:10 SLIDINGWINDOW:5:20 MINLEN:50
+done
 
 # gz output files
 pigz -p8 "$name"_sortmerna_forward_paired.fq
